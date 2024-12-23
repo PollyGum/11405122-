a677c970c40533079e96587ab1d2e3475f71d8c2
PollyGum <146710112+PollyGum@users.noreply.github.com>
2024-12-23 15:58:49 +0300

    Update main.cpp

--- 06/main.cpp
+++ 06/main.cpp
@@ -1,15 +1,24 @@
-/** 
- * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			      1 if num is lower than the picked number
- *               otherwise return 0
- * int guess(int num);
- */
+int guess(int num);
 
 class Solution {
 public:
     int guessNumber(int n) {
-        
+        int low = 1, high = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // Предотвращаем переполнение
+            int result = guess(mid);
+
+            if (result == 0) {
+                return mid; // Угадали число
+            } else if (result == -1) {
+                high = mid - 1; // Загаданное число меньше
+            } else {
+                low = mid + 1; // Загаданное число больше
+            }
+        }
+
+        // Здесь мы никогда не должны оказаться, если входные ограничения соблюдены
+        return -1; 
     }
-}
+};
